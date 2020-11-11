8b2fc54cf53da24bb00efaa3e710527a317bed58
stsnx <69145525+stsnx@users.noreply.github.com>
2020-11-11 10:50:17 +0700

    Update Source.cpp

--- Week_11/Source.cpp
+++ Week_11/Source.cpp
@@ -13,28 +13,28 @@ void maketable(vector<seat> tb) {
 		tab[tb[i].row - 97][tb[i].column - 1] = 1;
 	}
 	for (int j = 0; j < 1; j++) cout << "   ";
-	for (int j = 0; j < 10; j++) cout << j+1<<" ";
+	for (int j = 0; j < 9; j++) cout << j + 1 << " ";
 	cout << endl;
 	for (int j = 0; j < 1; j++) cout << "  ";
-	for (int j = 0; j < 10; j++) cout << "--";
+	for (int j = 0; j < 9; j++) cout << "--";
 	cout << endl;
-	for (int i = 0; i < 10; i++) {
-		cout << char(65+i) << " |";
-		for (int j = 0; j < 10; j++) {
+	for (int i = 0; i < 9; i++) {
+		cout << char(65 + i) << " |";
+		for (int j = 0; j < 9; j++) {
 			if (tab[i][j]) cout << "x|";
 			else cout << " |";
 		}
 		cout << endl;
 		for (int j = 0; j < 1; j++) cout << "  ";
-		for (int j = 0; j < 10; j++) cout << "--";
+		for (int j = 0; j < 9; j++) cout << "--";
 		cout << endl;
 	}
 }
-int main(){
+int main() {
 	vector<seat> tb;
 	maketable(tb);
-	
-	while(1)
+
+	while (1)
 	{
 		char d;
 		char emp[2];
@@ -42,20 +42,20 @@ int main(){
 		cout << "Do you want to reserve a seat? (y/n) ";
 		cin >> d;
 		if (d == 'y') {
-			
+
 			cout << "Enter your seat(Example 7e) : ";
 			cin >> emp;
 			Temp.row = emp[1];
-			Temp.column = emp[0]-48;
-			if (!tab[Temp.row - 97][Temp.column-1]) {
+			Temp.column = emp[0] - 48;
+			if (!tab[Temp.row - 97][Temp.column - 1]) {
 				tb.push_back(Temp);
 				maketable(tb);
 			}
 			else cout << "Error or your seat is reserved." << endl;
 		}
 		else if (d == 'n') {
-		cout << "thank you, please come back again." << endl;
-		cout  << "------------------------------------" << endl;
+			cout << "thank you, please come back again." << endl;
+			cout << "------------------------------------" << endl;
 		}
 		else cout << "Error : invalid value, please try again" << endl;
 	}
